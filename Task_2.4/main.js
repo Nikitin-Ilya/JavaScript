d7c4cc233e99a48d09f186e35b2da0a9434b0000
POFIGISTER <mr.nikiti81@gmail.com>
2022-11-22 14:05:35 +0200

    add weather forecast for 5 days

--- Task_2.4/main.js
+++ Task_2.4/main.js
@@ -1,17 +1,33 @@
-const widget = document.querySelector('#widget');
+const weatherForecastNow = document.querySelector('#weather-forecast-now');
+const weatherForecast = document.querySelector('#weather-forecast');
+
 const input = document.getElementById('input');
 
+input.addEventListener("keyup", e =>{
+  if(e.key == "Enter" && input.value != "" && weatherForecastNow && weatherForecast){
+    loadWeatherNow(input.value);
+    loadWeatherForecast(input.value);
+  }
+
+  if(input.value == "" && weatherForecastNow && weatherForecast){
+    weatherForecastNow.classList.remove('active');
+    weatherForecast.classList.remove('active');
+  }
+})
+
+// weather forecast now
+
 async function loadWeatherNow(city){
-  const serverWeatherNow = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e277e296d769f3ddc7c59e9d2a0b5173`;
-  const responseWeatherNow = await fetch(serverWeatherNow, {method: 'GET',})
+  const weatherNowAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e277e296d769f3ddc7c59e9d2a0b5173`;
+  const responseWeatherNow = await fetch(weatherNowAPI, {method: 'GET',})
 
   const responseResultWeatherNow = await responseWeatherNow.json();
 
   if (responseWeatherNow.ok) {
     getWeatherNow(responseResultWeatherNow);
   } else {
-    widget.classList.add('active');
-    widget.innerHTML = responseResultWeatherNow.message;
+    weatherForecastNow.classList.add('active');
+    weatherForecastNow.innerHTML = responseResultWeatherNow.message;
   }
 }
 
@@ -24,7 +40,7 @@ function getWeatherNow(data) {
   const weatherIcon = data.weather[0].icon;
 
   const template = `
-  <div class="whether-now" id="whether-now">
+  <div class="whether-now">
     <div class="whether-now__main">
       <div class="whether-now__temperature">
         ${temp}°C
@@ -40,16 +56,59 @@ function getWeatherNow(data) {
     </div>
   </div>`;
 
-  widget.classList.add('active');
-  widget.innerHTML = template;
+  weatherForecastNow.classList.add('active');
+  weatherForecastNow.innerHTML = template;
 }
 
-input.addEventListener("keyup", e =>{
-  if(e.key == "Enter" && input.value != "" && widget){
-    loadWeatherNow(input.value);
-  }
+// weather forecast for 5 days
 
-  if(input.value == "" && widget){
-    widget.classList.remove('active');
+async function loadWeatherForecast(city){
+  const weatherForecastAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=e277e296d769f3ddc7c59e9d2a0b5173`;
+  const responseWeatherForecast = await fetch(weatherForecastAPI, {method: 'GET',})
+
+  const responseResultWeatherForecast = await responseWeatherForecast.json();
+
+  if (responseWeatherForecast.ok) {
+    getWeatherForecast(responseResultWeatherForecast);
   }
-})
+}
+
+function getWeatherForecast(data) {
+  let template = ``;
+  const options = { weekday: 'short'};
+  let prevDay
+
+  data.list.forEach(element => {
+    const date = new Date(element.dt_txt);
+    const weekday = new Intl.DateTimeFormat('en-US', options).format(date);
+    if (prevDay == null || prevDay == weekday){
+      prevDay = weekday;
+      return;
+    }
+    prevDay = weekday;
+    const tempMax = Math.round(element.main.temp_max);
+    const tempMin = Math.round(element.main.temp_min);
+    const weatherStatus = element.weather[0].main;
+    const weatherIcon = element.weather[0].icon;
+  
+    template += `
+    <div class="whetherForecast">
+      <div class="whetherForecast__day">
+      ${weekday}
+      </div>
+      <div class="whetherForecast-icon">
+        <img src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="${weatherStatus}">
+      </div>
+      <div class="whetherForecast__status">
+        ${weatherStatus}
+      </div>
+      <div class="whetherForecast__temperature">
+        ${tempMax}°C
+        <div class="whetherForecast__temp-min">${tempMin}°C</div>
+      </div>
+    </div>`;
+  });
+
+  weatherForecast.classList.add('active');
+  weatherForecast.innerHTML = template;
+}
